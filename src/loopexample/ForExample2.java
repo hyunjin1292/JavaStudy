077d87e463097c3bd3053132fbe87153493f4467
jin <hyunjin1292@naver.com>
2022-12-19 17:07:51 +0900

    10_loop statement

---
+++ src/loopexample/ForExample2.java
@@ -0,0 +1,11 @@
+package loopexample;
+/*
+안녕하세요1 ~ 안녕하세요10 까지 차례로 출력하는 프로그램 만들기
+*/
+public class ForExample2 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("안녕하세요" + i);
+        }
+    }
+}
