e962078f524e1a21bd77d10d11e47cfb55ead8e7
jin <hyunjin1292@naver.com>
2022-12-20 14:11:25 +0900

    12_class

--- src/classpart/Student.java
+++ src/classpart/Student.java
@@ -5,4 +5,9 @@ public class Student {
     String studentName;
     int grade;
     String address;
+
+    // 메서드 추가
+    public void showStudentInfo() {
+        System.out.println(studentName + ", " + address);
+    }
 }
